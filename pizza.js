2ab36ac6cdcff6961772ad0fef2f19447e033929
ThijsDollekens <87063168+ThijsDollekens@users.noreply.github.com>
2021-08-10 10:04:43 +0200

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,70 @@
+//Pizza
+//console.log("Pizza test")
+
+const pizzaAmount = 2
+
+let make1Pizza = {
+    eerste: 'Maak 1x deeg',
+    tweede: 'Maak 1x beleg',
+    derder: 'Bak 1x pizza'
+};
+
+let make2Pizza = {
+    eerste: 'Maak 2x deeg',
+    tweede: 'Maak 2x beleg',
+    derder: 'Bak 2x pizza'
+};
+
+let make3Pizza = {
+    eerste: 'Maak 3x deeg',
+    tweede: 'Maak 3x beleg',
+    derder: 'Bak 3x pizza'
+};
+
+
+if (pizzaAmount == 1){
+    console.log(make1Pizza)
+}
+
+if (pizzaAmount == 2){
+    console.log(make2Pizza)
+}
+
+if (pizzaAmount == 3){
+    console.log(make3Pizza)
+}
+
+//Sushi
+
+const sushiAmount = 3
+
+let make1Sushi = {
+    eerste: 'Beleg rijst 1x',
+    tweede: 'Rol rijst 1x',
+    derder: 'Snijd sushi 1x'
+};
+
+let make2Sushi = {
+    eerste: 'Beleg rijst 2x',
+    tweede: 'Rol rijst 2x',
+    derder: 'Snijd sushi 2x'
+};
+
+let make3Sushi = {
+    eerste: 'Beleg rijst 3x',
+    tweede: 'Rol rijst 3x',
+    derder: 'Snijd sushi 3x'
+};
+
+
+if (sushiAmount == 1){
+    console.log(make1Sushi)
+}
+
+if (sushiAmount == 2){
+    console.log(make2Sushi)
+}
+
+if (sushiAmount == 3){
+    console.log(make3Sushi)
+}
